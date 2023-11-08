40c952192f66a92cb0c1ec66332b088e90de26b6
shwephuehmone25 <shwephue7889@gmail.com>
2023-11-08 13:20:07 +0630

    pdf bugs fix

--- controllers/post.js
+++ controllers/post.js
@@ -461,17 +461,18 @@ exports.savePostAsPDF = (req, res, next) => {
     orientation: "portrait",
     border: "10mm",
     header: {
-      height: "45mm",
+      height: "20mm",
       contents:
         '<h4 style="text-align: center;">PDF DOWNLOAD FROM medium.IO</h4>',
     },
     footer: {
-      height: "28mm",
+      height: "15mm",
       contents: {
-        first: "Cover page",
-        contents:
-          '<span style="color: #444; text-align: center;">@codehub.mm</span>',
-      },
+        first: 'Cover page',
+        2: 'Second page', 
+        default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', 
+        last: 'Last Page'
+    }
     },
   };
   Post.findById(id)
